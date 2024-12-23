bf6788371cc6f14263c8332a346fbe55fd43297c
hc421039 <nkhshc421039@g.neec.ac.jp>
2024-12-23 13:37:18 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,11 @@
+const score = 62
+
+if (score >= 90) {
+    console.log("Excellent")
+}else if(score >= 80) {
+    console.log("Good")
+}else if(score >= 60) {
+    console.log("Normal")
+}else {
+    console.log("Red")
+}
