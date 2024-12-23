a90e1ed079a8d739380d883b7755969f80aedbde
hc421039 <nkhshc421039@g.neec.ac.jp>
2024-12-23 14:19:23 +0900

    6-4-6

---
+++ 6-4/6-4-6.mjs
@@ -0,0 +1,16 @@
+const status = "地獄谷"
+
+switch (status) {
+    case "地獄谷":
+        
+    case "大湯沼":
+        console.log("登別")
+        break
+    case "ウポポイ":
+        
+    case "白老牛":
+        console.log("白老")
+        break
+    default:
+        console.log("対象外")
+}
