cfff1ef737ee64819ddbc3b7033c43eddbd8c8e2
hc421039 <nkhshc421039@g.neec.ac.jp>
2024-12-23 11:36:33 +0900

    6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,10 @@
+const a = 10
+
+console.log(a > 9)
+console.log(a > 10)
+console.log(a >= 10)
+console.log(a >= 11)
+console.log(a < 11)
+console.log(a < 9)
+console.log(a <= 10)
+console.log(a <= 9)
