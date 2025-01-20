5c8653a2fe7f43abfa6b2bf25879ae1ea4d1a441
hc421039 <nkhshc421039@g.neec.ac.jp>
2025-01-20 13:44:50 +0900

    7-2-3

---
+++ 7-2/7-2-3.mjs
@@ -0,0 +1,5 @@
+import {calc23A, calc23B } from "./7-2-3_export.mjs"
+const result1 = calc23A(10, 5)
+console.log(result1)
+const result2 = calc23B (10, 5)
+console.log(result2)
---
+++ 7-2/7-2-3_export.mjs
@@ -0,0 +1,6 @@
+export const calc23A = (a, b) => {
+    return a * b
+}
+   export const calc23B = (a, b) => {
+    return a / b
+}
