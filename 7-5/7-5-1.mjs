8c00822bd9e97e830257f980f99e003db4e3e5c4
hc421039 <nkhshc421039@g.neec.ac.jp>
2025-01-20 15:21:14 +0900

    7-5-1

---
+++ 7-5/7-5-1.mjs
@@ -0,0 +1,22 @@
+const promiseSetTimeout = (timeout) =>
+    new Promise((resolve, reject) => {
+    setTimeout(() => {
+    // この例では処理が完了したことを返すだけなのでresolveには値を渡さない
+    resolve()
+    }, timeout)
+    })
+
+const promise751 = () => {
+    promiseSetTimeout(10000)
+    .then(() => {
+    console.log("★", new Date())
+    return promiseSetTimeout(10000)
+    })
+    .then(() => {
+    console.log("◆", new Date())
+    return promiseSetTimeout(5000)
+    })
+   }
+   console.log("start", new Date())
+   promise751()
+   console.log("end", new Date())
