39eb7bc5e48174f291df548b0583814783713d13
hc421039 <nkhshc421039@g.neec.ac.jp>
2025-01-27 12:07:38 +0900

    8-1-1

---
+++ server/api/8-1/controller.mjs
@@ -0,0 +1,12 @@
+const get811 = (req, res) => {
+    res.send(
+        JSON.stringify({
+        code: req.query.code,
+        name: "情報太郎",
+        }),
+    )
+}
+
+export const q8_1Controller = {
+    get811,
+}
